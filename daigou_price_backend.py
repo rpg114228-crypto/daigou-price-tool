03840e5d18de9dbd845e807c5e5859df49161eb6
rpg114228-crypto <rpg114228@gmail.com>
2026-06-17 12:46:15 +0800

    Fix GitHub Pages local backend access

--- daigou_price_backend.py
+++ daigou_price_backend.py
@@ -204,6 +204,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Access-Control-Allow-Private-Network", "true")
         self.send_header("Content-Length", str(len(data)))
         self.end_headers()
         self.wfile.write(data)
